79e18a8294f245a164cccf2716e232b0375164a8
Mauricio Luiz <mauriciojunio@gmail.com>
2018-07-03 23:02:27 -0300

    adiciona crud agenda e middleware de altentificação

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const consign  = require('consign');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const expressSession = require('express-session');
+const methodOverride = require('method-override');
+const error = require('./middlewares/error');
 const app = express();
-
+  
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(cookieParser('ntalk'));
 app.use(expressSession());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(express.static(path.  join(__dirname, 'public')));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 consign({})
   .include('models')
@@ -20,6 +23,9 @@ consign({})
   .then('routes')
   .into(app)
 ;
+
+app.use(error.notFound);
+app.use(error.serverError);
 
 app.listen(3000, () => {
   console.log('Ntalk no ar');
--- controllers/contatos.js
+++ controllers/contatos.js
@@ -2,8 +2,40 @@ module.exports = (app) => {
     const ContatosController = {
         index : function(req, res){
             const { usuario } = req.session;
-            res.render('contatos/index', {usuario});
+            const { contatos } = usuario;
+            res.render('contatos/index', {usuario, contatos});
         },
+        create : function(req, res){
+            const { contato } = req.body;
+            const { usuario } = req.session;
+
+            usuario.contatos.push(contato);
+            res.redirect('/contatos');
+        },
+        show : function(req, res){
+            const { id } = req.params;
+            const { usuario } = req.session;
+            const contato  = usuario.contatos[id]; 
+            res.render('contatos/show', {id, contato});
+        },
+        edit : function(req, res){
+            const { id } = req.params;
+            const { usuario } = req.session;
+            const contato  = usuario.contatos[id];
+            res.render('contatos/edit', {id, contato, usuario});
+        },
+        update : function(req, res){
+            const { contato } = req.body;
+            const { usuario } = req.session;
+            usuario.contatos[req.params.id] = contato;
+            res.redirect('/contatos');
+        },
+        destroy : function(req, res){
+            const { id } = req.params;
+            const { usuario } = req.session;
+            usuario.contatos.splice(id, 1);
+            res.redirect('/contatos');
+        }
     };
     return ContatosController;
 };
