935c2ca1cabc0b5603d4ddc1f3f191a533cae6ab
Mauricio Luiz <mauriciojunio@gmail.com>
2018-07-06 00:28:35 -0300

    adiciona socket io e uma conversa simples de chat

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const path    = require('path');
+const path  = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 const consign  = require('consign');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -7,6 +9,8 @@ const expressSession = require('express-session');
 const methodOverride = require('method-override');
 const error = require('./middlewares/error');
 const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
   
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -27,6 +31,14 @@ consign({})
 app.use(error.notFound);
 app.use(error.serverError);
 
-app.listen(3000, () => {
+io.on('connection', (client) => {
+  client.on('send-server', (data) => {
+      const resposta = `<b>${data.nome}:</b> ${data.msg}<br>`;
+      client.emit('send-client', resposta);
+      client.broadcast.emit('send-client', resposta);
+  });
+});
+
+server.listen(3000, () => {
   console.log('Ntalk no ar');
 });
