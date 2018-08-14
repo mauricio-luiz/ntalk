b2460aff8876de7ba494d29e10c865a8c48d5cba
mauricio <mauriciojunio@gmail.com>
2018-08-14 20:02:07 -0300

    adiciona redis para armazenamento da conversa

--- events/chat.js
+++ events/chat.js
@@ -1,36 +1,54 @@
+const redis = require('redis').createClient();
+
 module.exports = (app, io) => {
-    const onlines = {};
+    
     io.on('connection', (client) => {
         const {session} = client.handshake;
         const {usuario} = session;
 
-        onlines[usuario.email] = usuario.email;
-        for(let email in onlines){
-            client.emit('notify-onlines', email);
-            client.broadcast.emit('notify-onlines', email);
-        }
-
-        client.on('send-server', (hashDaSala, msg) => {
-            const novaMensagem = { email: usuario.email, sala: hashDaSala};
-            const resposta = `<b>${usuario.nome}:</b> ${msg}<br>`;
-            session.sala =  hashDaSala;
-            client.broadcast.emit('new-message', novaMensagem);
-            io.to(hashDaSala).emit('send-client', resposta);
+        redis.sadd('onlines', usuario.email, () =>{
+            redis.smembers('onlines', (err, emails) => {
+                emails.forEach((email) => {
+                    client.emit('notify-onlines', email);
+                    client.broadcast.emit('notify-onlines', email);
+                });
+            });
         });
 
-        client.on('create-room', (sala) => {
-            session.sala = sala;
-            client.join(sala);
+        client.on('send-server', (hashDaSala, msg) => {
+            const resposta = `<b>${usuario.nome}:</b> ${msg}<br>`;
+            const novaMensagem = { email: usuario.email, sala: hashDaSala };
+            console.log('send-server', hashDaSala, resposta);
+            redis.lpush(hashDaSala, resposta, () => {
+                client.broadcast.emit('new-message', novaMensagem);
+                io.to(hashDaSala).emit('send-client', resposta);
+            });            
+        });
+
+        client.on('create-room', (hashDaSala) => {
+            session.sala = hashDaSala;
+            client.join(hashDaSala);
+            const resposta = `<b>${usuario.nome}:</b> entrou.<br>`;
+            console.log('create room', hashDaSala, resposta);
+            redis.lpush(hashDaSala, resposta, () => {
+                redis.lrange(hashDaSala, 0, -1, (err, msgs) => {
+                    msgs.forEach((msg) => {
+                        io.to(hashDaSala).emit('send-client', msg);
+                    });
+                });
+            });
         });
 
         client.on('disconnect', () =>{
-            const {sala} = session;
+            const { sala } = session;
             const resposta = `<b>${usuario.nome}:</b> saiu.<br>`;
-            delete onlines[usuario.email];
-            session.sala = null;
-            client.leave(sala);
-            client.broadcast.emit('notify-offlines', usuario.email);
-            io.to(sala).emit('send-client', resposta);
+            redis.lpush( sala, resposta, () => {
+                session.sala = null;
+                redis.srem('onlines', usuario.email);
+                client.leave(sala);
+                client.broadcast.emit('notify-offlines', usuario.email);
+                io.to(sala).emit('send-client', resposta);
+            });            
         });
     });
 };
